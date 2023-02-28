23e4f236bede7ed5d8a0f7eda1e2ffeebbf0aee1
Marcos Vera <mvera3900@gmail.com>
2023-02-28 06:56:07 -0300

    Primer guardado del currículum

---
+++ JS/function.js
@@ -0,0 +1,13 @@
+document.getElementById("HUELLAS").addEventListener("click", function(){
+    document.getElementById("HUELLAS").style.backgroundColor="aqua";
+});
+
+document.getElementById("Durlock").addEventListener("click", function(){
+    document.getElementById("Durlock").style.backgroundColor="aqua";
+});
+document.getElementById("STARS COPIER'S").addEventListener("click", function(){
+    document.getElementById("STARS COPIER'S").style.backgroundColor="aqua";
+});   
+document.getElementById("ELECTRICISTA").addEventListener("click", function(){
+        document.getElementById("ELECTRICISTA").style.backgroundColor="aqua";
+});
